60e2d073fd1d13a8388313b6eaf4efc9993b0158
Praveen Sharma <piyush20112@gmail.com>
2025-02-28 10:51:02 +0530

    [Praveen] ADD HarryAge

---
+++ HarryAge.java
@@ -0,0 +1,8 @@
+public class HarryAge {
+    public static void main(String[] args) {
+        int bYear = 2000;
+        int pYear = 2024;
+        int age =pYear - bYear;
+        System.out.print("Harry's age in "+pYear+" is "+age);
+    }
+}
