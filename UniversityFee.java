ffb3a38ff2a0b12f7387eb82841fba6bdd01c7e8
Praveen Sharma <piyush20112@gmail.com>
2025-03-01 10:22:55 +0530

    [Praveen] ADD add University Fee

---
+++ UniversityFee.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+
+public class UniversityFee {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int courseFee = 125000;
+        System.out.println("The University is charging the student a fee of INR " + courseFee + " for the course.");
+    }
+}
